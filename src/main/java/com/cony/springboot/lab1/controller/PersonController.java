82cbc6ae71d8c4bb18c618d140e3c261f9c6f22d
conyyy833 <conyyy833@gmail.com>
2020-10-29 00:53:09 +0800

    build.yml015

--- src/main/java/com/cony/springboot/lab1/controller/PersonController.java
+++ src/main/java/com/cony/springboot/lab1/controller/PersonController.java
@@ -4,11 +4,14 @@ import com.cony.springboot.lab1.dao.PersonRepository;
 import com.cony.springboot.lab1.entity.Person;
 import com.cony.springboot.lab1.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,6 +41,7 @@ public class PersonController {
         List<Person> personList = personService.personList();
         if(personList!=null)
         {
+
             return new ResponseEntity(personList,HttpStatus.OK);
         }
         else
@@ -54,7 +58,11 @@ public class PersonController {
         Person person1 = personService.save(person);
         if(person1!=null)
         {
-            return new ResponseEntity(person1,HttpStatus.CREATED);
+            HttpHeaders headers=new HttpHeaders();
+            List<String> value=new ArrayList<>();
+            value.add("https://cony-person-lab1.herokuapp.com/persons/"+person1.getId());
+            headers.put(HttpHeaders.LOCATION,value);
+            return new ResponseEntity(person1,headers,HttpStatus.CREATED);
         }
         else
             return new ResponseEntity("error",HttpStatus.NOT_FOUND);
